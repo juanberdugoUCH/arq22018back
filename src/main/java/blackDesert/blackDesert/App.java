8ab399b940a07592bc41fa3dc74f6dcf63a49691
Gonzalo Sánchez <gonzafull14@gmail.com>
2018-10-05 22:43:14 -0300

    Proyect created.

---
+++ src/main/java/blackDesert/blackDesert/App.java
@@ -0,0 +1,13 @@
+package blackDesert.blackDesert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
