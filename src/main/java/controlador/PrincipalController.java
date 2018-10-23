fd55143752c3829f73bb428307be7b252e712b18
nicolaspico <patricionicolaspico@gmail.com>
2018-10-22 23:54:58 -0300

    se crea el servicio y el controlador

---
+++ src/main/java/controlador/PrincipalController.java
@@ -0,0 +1,29 @@
+package controlador;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import bl.PrincipalService;
+import spring.springGET;
+
+@RestController
+public class PrincipalController
+{
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public springGET greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new springGET(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    @RequestMapping("/GetData")
+    public String GetData() {
+    	PrincipalService bl = new PrincipalService();
+    	bl.GetData();
+    	return "Servicio";
+    }
+}
