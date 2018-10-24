0cc0081fd78f84966d1970550d6508cc450237e2
nicolaspico <patricionicolaspico@gmail.com>
2018-10-24 17:03:01 -0300

    Se agrega el metodo en controlador para llamar al servicio

--- src/main/java/spring/springController.java
+++ src/main/java/spring/springController.java
@@ -1,9 +1,13 @@
 package spring;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import bl.PrincipalService;
+import model.PrincipalModel;
 
 @RestController
 public class springController {
@@ -14,5 +18,11 @@ public class springController {
     public springGET greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new springGET(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    @RequestMapping("/getData")
+    public ArrayList<PrincipalModel> getData() {
+       PrincipalService service = new PrincipalService();
+       return service.GetDataMock();
     }
 }
