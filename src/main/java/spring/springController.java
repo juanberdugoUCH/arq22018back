de252a6b9a1332b666f4a070e61a0e2c54abf63d
nicolaspico <patricionicolaspico@gmail.com>
2018-10-31 19:29:43 -0300

    fix controller

--- src/main/java/spring/springController.java
+++ src/main/java/spring/springController.java
@@ -2,9 +2,15 @@ package spring;
 
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import bl.PrincipalService;
 import model.PrincipalModel;
@@ -20,6 +26,7 @@ public class springController {
                             String.format(template, name));
     }
     
+    @CrossOrigin(origins = "http://localhost:8080/getData")
     @RequestMapping("/getData")
     public ArrayList<PrincipalModel> getData() {
        PrincipalService service = new PrincipalService();
@@ -30,5 +37,15 @@ public class springController {
     public ArrayList<PrincipalModel> getPuntos() {
        PrincipalService service = new PrincipalService();
        return service.GetPuntosMock();
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("");
+            }
+        };
     }
 }
